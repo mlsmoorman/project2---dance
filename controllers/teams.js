92b135a78776968f55611bf0f770fc90ce0e14d7
mlsmoorman <melissa.s.moorman@gmail.com>
2023-10-30 15:33:51 -0500

    added show page

--- controllers/teams.js
+++ controllers/teams.js
@@ -4,14 +4,31 @@ module.exports = {
     index,
     new: newTeam,
     create,
+    show
 };
+
+async function show(req, res) {
+    console.log('<======THIS IS RUNNING THE SHOW FUNCTION=====>')
+    try {
+        const teamDocument = await TeamModel.findById(req.params.id)
+        console.log(teamDocument)
+        res.render("teams/show", { team: teamDocument })
+    } catch(err) {
+        console.log(err)
+        res.send(err)
+    }
+}
 
 async function index(req, res) {
     try  {
         console.log('<=====RUNNING THE TEAMS/INDEX FUNCTION=====>')
         const teamDocuments = await TeamModel.find({});
+    
         console.log("teamDocs", teamDocuments)
-        res.render("/teams/index", { teamDocs: teamDocuments });
+        
+        res.render("teams/index", { teamDocs: teamDocuments });
+
+
     } catch (err) {
         console.log(err);
         res.send(err);
@@ -30,7 +47,7 @@ async function create(req, res, next) {
     console.log(req.user)
     try {
         const teamDoc = await TeamModel.create(req.body);
-        res.redirect("/teams")
+        res.redirect("teams")
     } catch(err) {
         console.log(err);
         res.send(err);
